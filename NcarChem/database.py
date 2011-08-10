dd93106a9926c11df0d7bf8cd780cf533cec8986
Ryan Orendorff <ryano@ucar.edu>
2011-08-10 03:52:40 -0600

    Documented code

--- NcarChem/database.py
+++ NcarChem/database.py
@@ -17,6 +17,7 @@
 
 #### Inter-package
 import datafile
+import data
 
 ## PSQL interface
 import psycopg2 ## PostGreSQL module, http://www.initd.org/psycopg/
@@ -37,12 +38,16 @@ import sys
 ## --------------------------------------------------------------------------
 
 def _loadFile(file_path, dbname, host, user, password, dbstart):
+  """
+  Loads a .asc file with a header into a sql database for testing.
+  """
   nfile = datafile.NRTFile(file_path)
 
   SQL_CMDS = nfile.getSql()
   labels = nfile.labels
   data = nfile.data
 
+  ## Create a new test database
   conn = psycopg2.connect(database=dbstart, \
                           user=user, \
                           host=host, \
@@ -53,6 +58,7 @@ def _loadFile(file_path, dbname, host, user, password, dbstart):
   cursor.close()
   conn.close()
 
+  ## Join the new database
   conn = psycopg2.connect(database=dbname, \
                           user=user, \
                           host=host, \
@@ -67,12 +73,15 @@ def _loadFile(file_path, dbname, host, user, password, dbstart):
   for var in labels:
     VARS += "%s," % var.lower()
 
+  ## Add data into test dataabase
   INSERT_CMD = "INSERT INTO raf_lrt (" + VARS.rstrip(', ') + ") VALUES (%s);"
   for row in data:
     data_piece = ""
     for item in row:
       data_piece += "'%s'," % str(item)
     cursor.execute(INSERT_CMD % data_piece.rstrip(', '))
+
+  ## Close all connections to the database, will be picked up later.
   cursor.close()
   conn.close()
 
@@ -82,20 +91,37 @@ def _loadFile(file_path, dbname, host, user, password, dbstart):
 
 
 class NDatabaseManager(BaseManager):
+  """
+  Used for multiprocess purposes.
+  """
   pass
 
 
 class NDatabaseLiveUpdater(object):
+  """
+  Used to update the data inside an NVarSet with the newest data from the
+  server. This provides easy server use without knowing server/NVar
+  functions.
+  """
 
   def __init__(self, server=None, variables=None):
     self.server = server
     self._last_update_time = None
-    self._vars = variables
+
+    ## Get all the variables is they are not specified
+    if variables == None:
+      self._vars = data.NVarSet(server.variable_list)
+    else:
+      self._vars = variables  ## of type NVarSet
 
     current_time = server.getData(number_entries=1)
     self._last_update_time = current_time[0][0]
 
   def update(self):
+    """
+    Update attached variables with new data, and then sleep the server so it
+    polls less frequently.
+    """
     data = self.server.getData(start_time=self._last_update_time,\
                                variables=self._vars.keys())
 
@@ -107,7 +133,12 @@ class NDatabaseLiveUpdater(object):
 
 
 class NDatabase(object):
+  """
+  An interface to the EOL PostGresQL database. It is designed to return data
+  normally picked up by Aeros without knowing the SQL commands necessary.
+  """
 
+  ## All defaults used at RAF as of 10/08/11 02:48:30
   def __init__(self, host="eol-rt-data.guest.ucar.edu",
                      user="ads",
                      password="",
@@ -115,49 +146,63 @@ class NDatabase(object):
                      simulate_start_time=None,
                      simulate_fast=False,
                      simulate_file=None):
+    ## Database related
     self._database = database
     self._user = user
     self._password = password
     self._host = host
     self.variable_list = ()
     self._flight_info = None
+    self._conn = None
+    self._running = True
+    self._sql_bad_attempts = 0
+
+    ## Time (which can be spoofed with the simulate parameters
     self._start_time = datetime.datetime.now()
-    self._simulate_start_time = simulate_start_time
     self._current_time = simulate_start_time if simulate_start_time != None\
                     else self._start_time
-    self._conn = None
-    self._simulate_fast = simulate_fast if simulate_start_time != None else\
                           False
-    self._sql_bad_attempts = 0
-    self._simulate_start_db = None
-    self._running = True
 
+    ## Simulate variables
+    self._simulate_start_db = None
+    self._simulate_start_time = simulate_start_time
+    self._simulate_fast = simulate_fast if simulate_start_time != None else\
+
+    ## Error Checking
     if database == None:
       raise ValueError('Database must be specified')
 
+    ## Shorthand for C130 and GV
     if database == "C130" or database == "GV":
       self._database = "real-time-" + database
 
+    ## If we are simulating from a file, load the file with
+    ## a random database name
     if simulate_file != None:
       dbname = "test" + str(int(random.random() * 1000000))
       self._simulate_start_db = self._database
       tmp_db = self._database
       self._database = dbname
-      _loadFile(simulate_file, dbname, self._host, self._user, self._password, tmp_db)
+      _loadFile(simulate_file,
+                dbname, self._host, self._user, self._password, tmp_db)
 
+    ## Create first connection to database
     self.reconnect()
 
+    ## Grab variable list from server
     cursor = self._conn.cursor()
     cursor.execute("SELECT column_name FROM Information_Schema.Columns WHERE\
                     table_name='raf_lrt'")
     variable_list = cursor.fetchall()
 
-    for var in variable_list:
-      self.variable_list = var + self.variable_list
+    ## Variable list is originally reversed.
+    self.variable_list = variable_list.reverse()
 
+    ## Get flight information
     cursor.execute("SELECT * FROM global_attributes;")
     self._flight_info = dict(cursor.fetchall())
 
+    ## We are done
     cursor.close()
 
   def __del__(self):
@@ -166,22 +211,35 @@ class NDatabase(object):
     except Exception, e:
       pass
     if self._simulate_start_db != None and self._running == True:
-      import psycopg2 ## Load library back in before finally taking stuff down
-      conn = psycopg2.connect(database=self._simulate_start_db, \
-                              user=self._user, \
-                              host=self._host, \
-                              password=self._password)
-      conn.set_isolation_level(0)
-      cursor = conn.cursor()
-      cursor.execute("DROP DATABASE %s;" % self._database)
-      cursor.close()
-      conn.close()
+      try:
+        ## Load library back in before finally taking stuff down
+        import psycopg2
+        conn = psycopg2.connect(database=self._simulate_start_db, \
+                                user=self._user, \
+                                host=self._host, \
+                                password=self._password)
+        conn.set_isolation_level(0)
+        cursor = conn.cursor()
+        cursor.execute("DROP DATABASE %s;" % self._database)
+        cursor.close()
+        conn.close()
+      except Exception, e:
+        ## Did not work, but don't change error exit code
+        print "Could not clode simulated db"
 
   def stop(self):
+    """
+    Stop running the server. Use if in an infinite loop.
+    """
     self.__del__()
     self._running = False
 
   def reconnect(self):
+    """
+    Sometimes the connection goes stale and will not recognize when the server
+    has replaced its database with a fresh one. Hence this function should be
+    called periodically to update when these database transitions occur.
+    """
     try:
       self._conn = psycopg2.connect(database=self._database, \
                                     user=self._user, \
@@ -191,6 +249,9 @@ class NDatabase(object):
       print e
 
   def flying(self):
+    """
+    Is the airplane going faster than 50 m/s right now?
+    """
     speed = 0
     data = (self.getData(number_entries=1, variables=('tasx',)))
     if len(data) != 0:
@@ -201,6 +262,11 @@ class NDatabase(object):
       return False
 
   def sleep(self, sleep_time=0):
+    """
+    Used to wait for new data. If in simulation mode this increments time
+    forward.
+    """
+    ## Get the data rate from the server, usually 3 seconds
     if sleep_time == 0:
       sleep_time = int(self._flight_info['DataRate'])
 
@@ -210,13 +276,24 @@ class NDatabase(object):
       time.sleep(sleep_time)
 
   def getTimeStr(self):
+    """
+    Returns the most recent datapoint time with a Z attached to signify
+    Zulu time.
+    """
     return str(self._getSimulatedCurrentTime()) + "Z"
 
 
   def getFlightInformation(self):
+    """
+    Get the flight information. This is updated when a new flight is detected.
+    """
     return self._flight_info
 
   def _getSimulatedCurrentTime(self):
+    """
+    Get the most recent time from the data. Will give a simulated time if
+    in simulation mode.
+    """
     if self._simulate_fast:
       return ((self._current_time - self._simulate_start_time) \
              + self._simulate_start_time).replace(microsecond=0)
@@ -224,8 +301,17 @@ class NDatabase(object):
       return ((datetime.datetime.now() - self._start_time) \
              + self._simulate_start_time).replace(microsecond=0)
 
-  def getData(self, variables=None, start_time=None, end_time=None,\
+  def getData(self, variables=None,
+                    start_time=None, end_time=None,
                     number_entries=None):
+    """
+    Get data from the server for the selected variables, where the variables
+    are a tuple/list. Can be manipulated to get data from a range or just a
+    certain number of entries. The times can also be intervals such as
+    start_time = "-60 MINUTES".
+    """
+    ## Use server now function if not in simulation mode, otherwise perform
+    ## the SQL query with the simulated current time as the upper bound.
     NOW = str(self._getSimulatedCurrentTime()) \
           if self._simulate_start_time != None \
           else "NOW()"
@@ -233,9 +319,10 @@ class NDatabase(object):
     if isinstance(start_time, datetime.datetime):
       start_time = str(start_time)
 
+    ## Start building SQL command
     sql_command = "SELECT "
 
-
+    ## Variables to get. Will always get datetime.
     var_str = "datetime, "
     if variables != None:
       for var in variables:
@@ -244,12 +331,16 @@ class NDatabase(object):
         else:
           print >> sys.stderr, "Could not add variable %s, does not exist"\
                                 % var
-
     var_str = var_str.rstrip(', ')
+
+    ## All Aeros displayable data from raf_lrt
     sql_command += var_str + " FROM raf_lrt "
 
+    ## Open SQL connection
     cursor = self._conn.cursor()
 
+    ## TODO: Clean time interval part of server.getData
+    ## Start creating time interval string.
     time_interval = ""
     if   end_time == None and start_time != None and number_entries == None:
       ## Assume -# INTERVAL syntax, SQL style.
@@ -307,54 +398,105 @@ class NDatabase(object):
         self.reconnect()
 
     cursor.close()
-
     return data
 
   def getDatabaseStructure(self):
+    """
+    Get the database structure, return as string. See documentation for
+    the string formatting. This information goes into the header of
+    outputted files.
+    """
     cursor = self._conn.cursor()
-    cursor.execute("SELECT table_name from information_schema.tables WHERE table_type = 'BASE TABLE' and table_schema NOT IN ('pg_catalog', 'information_schema');")
+
+    ## Get a list of Tables
+    cursor.execute("SELECT table_name FROM information_schema.tables " +\
+                   "WHERE table_type = 'BASE TABLE' " +\
+                   "AND table_schema NOT IN " +\
+                     "('pg_catalog', 'information_schema');")
     tables = tuple([col[0] for col in cursor.fetchall()])
 
-    cursor.execute("SELECT T.TABLE_NAME, K.COLUMN_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS T INNER JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE K ON T.CONSTRAINT_NAME = K.CONSTRAINT_NAME WHERE T.CONSTRAINT_TYPE = 'PRIMARY KEY' -- AND T.TABLE_NAME = 'table_name' ORDER BY T.TABLE_NAME, K.ORDINAL_POSITION;")
+    ## Get a list of constrains on those tables, as a dict{tbl_name:constrain}
+    cursor.execute("SELECT t.table_name, k.column_name " + \
+                   "FROM information_schema.table_constraints " +\
+                   "T INNER JOIN information_schema.key_column_usage " +\
+                   "K ON T.CONSTRAINT_NAME = k.constraint_name " +\
+                   "WHERE T.CONSTRAINT_TYPE = 'PRIMARY KEY' -- " +\
+                   "AND T.TABLE_NAME = 'table_name' " +\
+                   "ORDER BY T.TABLE_NAME, K.ORDINAL_POSITION;")
     constraints = dict(cursor.fetchall())
+
+    ## Start building output string
     output = ""
     for table in tables:
+      ## Start building string for a specific table.
       tbl_string = "%s=" % table
-      cursor.execute("SELECT column_name,data_type,is_nullable,character_maximum_length, udt_name FROM INFORMATION_SCHEMA.COLUMNS WHERE  TABLE_NAME = '%s' ORDER BY ORDINAL_POSITION;" % table);
+
+      ## Get the listing of columns and their data types.
+      cursor.execute("SELECT " +\
+                     "column_name, data_type, is_nullable, " +\
+                     "character_maximum_length, udt_name " +\
+                     "FROM information_schema.columns " +\
+                     "WHERE TABLE_NAME = '%s' " +\
+                     "ORDER BY ordinal_position;" % table);
       columns = cursor.fetchall()
+
+      ## Each column string is (COLUMNS, (col1, type, null?), etc)
       tbl_string += "('COLUMNS',"
       for col in columns:
+        ## Get column information from returned string.
         col_name = col[0]
+
+        ## Unfortunately  data_type from SQL returns ARRAY instead of integer[]
+        ## or double precision[], and so these types must be reconstructed
+        ## from their raw variable types.
         if col[4] == "_int4":
           col_base_type = "integer[]"
         elif col[4] == "_float8":
           col_base_type = "double precision[]"
         else:
           col_base_type = col[1]
+
+        ## Similarly data_type does not give the length of a char array.
+        ## It must be reconstructed as character(#) from the SQL variable
+        ## character_maximum_length. Does not affect not char values.
         col_type = col_base_type + ('(' + str(col[3]) + ')' if col[3] != None else "")
+
+        ## Make some values forced to be something besides NULL
         col_null = 'NOT NULL' if col[2] == 'NO' else ''
+
+        ## (name, compiled_type, am I null?)
         tbl_string += "('%s','%s','%s')," % (col_name, col_type, col_null)
+
       tbl_string = tbl_string.rstrip(', ') + ')'
+
+      ## Add constraint if applicable
       if table in constraints:
         tbl_string += ";('CONSTRAINT', '%s')" % constraints[table]
+
+      ## Data in table goes after the % character
       tbl_string += '%'
+
+      ## Ignore raf_lrt, that is where the Aeros data comes from.
       if table != "raf_lrt":
         cursor.execute("SELECT * from %s;" % table);
         data = cursor.fetchall()
         data = "" if data == [] else tuple(data)
+
+        ## Data is just the string representation of a tuple, allows for
+        ## data to be imported using the eval() function
         tbl_string += str(data).replace("Uncorr'd Raw", "Uncorr''d Raw")
+
+      ## Add the table to the output string
       output += tbl_string + '\n'
+
+    ## Finally, we have all the tables, end query.
     cursor.close()
     return output.strip('\n')
-
-
 
 
 ## --------------------------------------------------------------------------
 ## Start command line interface (main)
 ## --------------------------------------------------------------------------
-
-
 if __name__ == "__main__":
 
   server = NDatabase(database="test",
@@ -366,4 +508,3 @@ if __name__ == "__main__":
                      simulate_file=sys.argv[1])
 
   print "Done loading"
-  time.sleep(124121)
--- NcarChem/datafile.py
+++ NcarChem/datafile.py
@@ -14,8 +14,14 @@
 ## --------------------------------------------------------------------------
 ## Imports and Globals
 ## --------------------------------------------------------------------------
+
+## Regular expressions to parse files.
 import re
+
+## Time
 import datetime
+
+## For sys.stderr
 import sys
 
 
@@ -24,84 +30,122 @@ import sys
 ## --------------------------------------------------------------------------
 
 def _SqlFromHeader(header):
+  """
+  Creates SQL commands from a header file.
+  """
+  ## Start of entire command list, including table creation and data entry.
   cmd_list = ()
   for cnt, line in enumerate(header.split('\n')):
+
+    ## only get information that starts with "#!"
     if len(line) > 0 and line[0] == "#":
       if len(line) > 1 and line[1] == "!":
+        ## Split into table name; columns, rules, and constraints; and data
         tbl = re.match("^#!\s*(\w+)\s*=\s*(.*)%(.*)$", line)
         if tbl:
           tbl_name = tbl.groups()[0]
           tbl_info = ()
           for info in tbl.groups()[1].split(';'):
             tbl_info += (eval(info), )
+
           if tbl.groups()[2] != "":
             tbl_data = eval(tbl.groups()[2])
           else:
             tbl_data = None
           tbl_cmds = [col[0] for col in tbl_info]
         else:
-          print >>sys.stderr, "Table information line improperly formatted. Line %s" % cnt
+          print >>sys.stderr, "Table information line " + \
+                              "improperly formatted. Line %s" % cnt
 
+        ## First command for a table
         SQL_CMD = "CREATE TABLE %s (" % tbl_name
 
+        ## Create table SQL command var sets, including name, type, and null
         VARS = ""
         if "COLUMNS" in tbl_cmds:
           for col in tbl_info[tbl_cmds.index('COLUMNS')][1:]:
             VARS += "%s %s %s, " % (col[0], col[1], col[2])
 
-        COLUMNS_DATA_TYPE = [[col[0], col[1]] for col in  tbl_info[tbl_cmds.index('COLUMNS')][1:]]
+        ## Get a list of the column datatypes for later use.
+        COLUMNS_DATA_TYPE = [[col[0], col[1]] for col in
+                              tbl_info[tbl_cmds.index('COLUMNS')][1:]]
 
+        ## Add contrains to end of table creation command
         if "CONSTRAINT" in tbl_cmds:
           info = tbl_info[tbl_cmds.index('CONSTRAINT')][1:]
           VARS += "CONSTRAINT %s_pkey PRIMARY KEY (%s)" % (tbl_name,info[0])
 
+        ## Finish table creation command, add to command list
         VARS = VARS.rstrip(', ')
-
         SQL_CMD += "%s);" % VARS
+
         cmd_list +=  (SQL_CMD,)
+
+        ## Create rule SQL command
         if "RULE" in tbl_cmds:
           info = tbl_info[tbl_cmds.index("RULE")][1:]
-          cmd_list += ("CREATE RULE %s AS ON %s TO %s DO %s;" % (info[0], info[1], tbl_name, info[2]),)
+          cmd_list += ("CREATE RULE %s AS ON %s TO %s DO %s;" % \
+                        (info[0], info[1], tbl_name, info[2]),)
 
+        ## Create table insertion commands
         if tbl_data != None:
           INSERT_CMD="INSERT INTO %s VALUES (" % tbl_name
+
+          ## Format values, all flanked with ''
           for values in tbl_data:
             DATA = ""
             for counter, value in enumerate(values):
+              ## change input string based on column datatype
               if COLUMNS_DATA_TYPE[counter][1] == "text":
                 DATA += "'%s'," % value
               elif COLUMNS_DATA_TYPE[counter][1] == "double precision":
                 DATA += "%s," % value
               elif COLUMNS_DATA_TYPE[counter][1] == "integer":
                 DATA += "%s," % value
-              elif COLUMNS_DATA_TYPE[counter][1] == "timestamp without time zone":
-                DATA += "TIMESTAMP '%s'" % value
+              elif COLUMNS_DATA_TYPE[counter][1] == "timestamp " + \
+                                                    "without time zone":
+                DATA += "TIMESTAMP '%s'," % value
               elif "character" in COLUMNS_DATA_TYPE[counter][1]:
                 DATA += "'%s'," % value
               elif "[]" in COLUMNS_DATA_TYPE[counter][1]:
+                ## Arrays are surrounded by {}
                 DATA += "'%s'," % str(value).replace('[', '{').replace(']', '}')
               else:
-                print >>sys.stderr, "Problem creating insert statement for value %s, unknown type" % value
+                print >>sys.stderr, "Problem creating insert statement " +\
+                                    "for value %s, unknown type" % value
+
             DATA = DATA.rstrip(', ')
             cmd_list += (INSERT_CMD + DATA + ");", )
 
+  ## Finally done creating insert commands
   return cmd_list
 
 def _concatTime(labels, data):
+  """
+  Take Year,Month,...,Second columns and combine them into a datetime type
+  string.
+  """
+  ## Compact date columns
   if labels[0] == "YEAR":
     labels = ['DATE'] + labels[3:]
     data = [ ["%s-%s-%s" % (col[0], col[1], col[2])] + col[3:] for col in data]
+
+  ## Compact time columns
   if labels[1] == "HOUR":
     labels = ['DATETIME'] + labels[4:]
     data = [ [datetime.datetime.strptime("%s %s:%s:%s" % (col[0], col[1], col[2], col[3]), '%Y-%m-%d %H:%M:%S')] + col[4:] for col in data]
   elif labels[1] == "UTC":
     labels = ['DATETIME'] + labels[4:]
     data = [ datetime.datetime.strptime("%s %s" % (col[0], col[1]), '%Y-%d-%m %H:%M:%S') + col[2:] for col in data]
-  return tuple(labels), data
+
+  return tuple(labels), data  ## data is an list, not tuple
 
 def _parseIntoHeaderLabelsData(file_str):
   try:
-    file_pieces = re.match(r"^(#.*?\n)?(?=[^#]*?\n)(.*?)\n(.*)$", file_str, re.S)
+    ## Get pieces based on structure
+    ## name=tbl_information1;tbl_information2%tbl_data
+    file_pieces = re.match(r"^(#.*?\n)?(?=[^#]*?\n)(.*?)\n(.*)$",
+                           file_str, re.S)
     header = file_pieces.groups()[0]
     labels = file_pieces.groups()[1].upper().split(',')
     data   = [row.split(',') for row in file_pieces.groups()[2].split('\n') if row != ""]
@@ -117,6 +161,10 @@ def _parseIntoHeaderLabelsData(file_str):
 
 
 class NRTFile(object):
+  """
+  A class to deal with .asc files. Files that exist can be loaded into this
+  class and they can be written out using this class.
+  """
   def __init__(self, file_name=""):
     self.header = ""
     self.labels = ""
@@ -138,21 +186,40 @@ class NRTFile(object):
       self.file_name = file_name
 
   def setHeader(self, sql_structure):
+    """
+    Set SQL header structure using SQL database structure string, see
+    NDatabase.getDatabaseStructure.
+    """
     for line in sql_structure.split('\n'):
       self.header += "#! %s\n" % line
     self.header = self.header.rstrip('\n')
 
   def setLabels(self, labels):
+    """
+    Set variable list. Do not include datetime unless it was specifically
+    added. This is a list/tuple.
+    """
     self.labels = labels
     self.variables = labels[1:]
 
   def setData(self, data):
+    """
+    Add in data matrix.
+    """
     self.data = data
 
   def getSql(self):
+    """
+    Return the header information parsed as SQL command list.
+    """
     return _SqlFromHeader(self.header)
 
   def write(self, file_name="", header=None, labels=None, data=None):
+    """
+    Write file to destination, with any combination of header, label, and
+    data information.
+    """
+    ## Replace information if provided to function
     if header != None:
       self.setHeader(header)
       header=self.header
@@ -168,10 +235,14 @@ class NRTFile(object):
       file_name = self.file_name
     self.file_name = file_name
 
+    ## Files always start with the time in the label
     label_str = 'YEAR,MONTH,DAY,HOUR,MINUTE,SECOND'
+
+    ## Add in the rest of the variables to the labels list
     for variable in labels[1:]:
       label_str += ',%s' % variable.upper()
 
+    ## Start outputting data
     data_str = ""
     for row in data:
       line = ""
@@ -182,19 +253,19 @@ class NRTFile(object):
           line += '%s,' % str(value)
       data_str += line.rstrip(', ') + '\n'
 
+    ## Try really hard to write the file.
     try:
       f = open(file_name, 'w')
     except Exception, e:
       print >>sys.stderr, "Could not open file %s for writing." % file_name
-      print e
       return
 
     try:
       f.write(header + '\n' + label_str + '\n' + data_str)
     except Exception, e:
       print >>sys.stderr, "Could not write to file %s" % file_name
-      print e
 
+    ## Only needed if open did not work.
     try:
       f.close()
     except:
@@ -208,13 +279,16 @@ class NRTFile(object):
 ## --------------------------------------------------------------------------
 
 if __name__ == "__main__":
+  """
+  Test of NRTFile, where the first command line parameter is the input file.
+  """
   nfile = NRTFile(sys.argv[1])
 
-  #print "-----------------------------------------------------------------" + \
-        #"\nSQL Commands\n" + \
-        #"-----------------------------------------------------------------"
-  #if nfile.header != None:
-    #print nfile.getSql()
+  print "-----------------------------------------------------------------" + \
+        "\nSQL Commands\n" + \
+        "-----------------------------------------------------------------"
+  if nfile.header != None:
+    print nfile.getSql()
   print "-----------------------------------------------------------------" + \
         "\nLabels\n" + \
         "-----------------------------------------------------------------"
