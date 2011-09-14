2e6b1a49c7d5012abf0486397195c2c76d1e5eac
Ryan Orendorff <ryano@ucar.edu>
2011-09-14 04:49:51 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name="NCAR Flight Monitor",
+      version='0.2',
+      description='Monitor in flight data for RAF field projects in real time.',
+      author='Ryan Orendorff',
+      author_email='ryan@rdodesigns.com',
+      url='http://github.com/rdodesigns/NCARFlightMonitor',
+      packages=find_packages(),
+      install_requires=[
+        'psycopg2>=2.4.2',
+        'Twisted>=11.0.0'
+        ]
+)
